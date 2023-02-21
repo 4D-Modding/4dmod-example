2404f49f771d53760f3867a4c74da37f0ea0711c
Tr1NgleDev <steamkyzagames2@gmail.com>
2023-02-22 01:27:22 +0800

    release or smth

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 #define GLEW_STATIC // DO NOT REMOVE THIS OR ELSE GLEW WILL NOT WORK
 
+// Mod Name. Make sure its same as mod's folder name
+#define MOD_NAME "4DModExample"
+#define MOD_VER "1.0"
+
 #include <Windows.h>
 #include <cstdio>
 #include <4dm.h>
 using namespace fdm;
 #include <MinHook.h>
+
+
 
 void(__thiscall* GameState_init)(GameState* self, StateManager& s);
 void __fastcall GameState_init_H(GameState* self, StateManager& s)
@@ -14,7 +20,7 @@ void __fastcall GameState_init_H(GameState* self, StateManager& s)
 	glfwInit(); // for glfw functions to work
 	glewInit(); // for glew/gl functions to work
 
-	// Your code that runs at first frame here (it calls when you load into world)
+	// Your code that runs at first frame here (it calls when you load into the world)
 
 	GameState_init(self, s);
 }
@@ -22,7 +28,7 @@ void __fastcall GameState_init_H(GameState* self, StateManager& s)
 void(__thiscall* Player_update)(Player* self, GLFWwindow* window, World* world, double dt);
 void __fastcall Player_update_H(Player* self, GLFWwindow* window, World* world, double dt) 
 {
-	// Your code that runs every frame here (it only calls when you play in world, because its Player function)
+	// Your code that runs every frame here (it only calls when you play in world, because its Player's function)
 
 	Player_update(self, window, world, dt);
 }
@@ -30,7 +36,7 @@ void __fastcall Player_update_H(Player* self, GLFWwindow* window, World* world, 
 bool(__thiscall* Player_keyInput)(Player* self, GLFWwindow* window, int key, int scancode, int action, int mods);
 bool __fastcall Player_keyInput_H(Player* self, GLFWwindow* window, int key, int scancode, int action, int mods) 
 {
-	// Your code that runs when Key Input happens (check GLFW Keyboard Input tutorials)|(it only calls when you play in world, because its Player function)
+	// Your code that runs when Key Input happens (check GLFW Keyboard Input tutorials)|(it only calls when you play in world, because its Player's function)
 
 	return Player_keyInput(self, window, key, scancode, action, mods);
 }
@@ -43,14 +49,6 @@ DWORD WINAPI Main_Thread(void* hModule)
 	FILE* fp;
 	freopen_s(&fp, "CONOUT$", "w", stdout);
 #endif
-	// Initialize MinHook and make sure its initialized
-	auto mhInitialize = MH_Initialize();
-	if (mhInitialize != MH_OK)
-	{
-		FreeLibraryAndExitThread(reinterpret_cast<HMODULE>(hModule), 0);
-		return false;
-	}
-
 	// Hook to the GameState::init function
 	MH_CreateHook(reinterpret_cast<void*>(base + idaOffsetFix(0x50690)), reinterpret_cast<void*>(&GameState_init_H), reinterpret_cast<void**>(&GameState_init));
 
@@ -68,7 +66,5 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD _reason, LPVOID lpReserved)
 {
 	if (_reason == DLL_PROCESS_ATTACH)
 		CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)Main_Thread, hModule, 0, NULL);
-	else if (_reason == DLL_PROCESS_DETACH)
-		MH_DisableHook(MH_ALL_HOOKS);
 	return TRUE;
 }
